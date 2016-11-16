e4e267cfc1d3c1cffcd6542ad0e23ffb49b09be4
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-17 01:43:03 +0530

    changed getResource() to getResourceAsStream to fix URI issue

--- java/merchants-guide-to-galaxy/src/main/java/com/codingkapoor/merchantsguidetogalaxy/util/UserInputFileReader.java
+++ java/merchants-guide-to-galaxy/src/main/java/com/codingkapoor/merchantsguidetogalaxy/util/UserInputFileReader.java
@@ -1,30 +1,27 @@
 package com.codingkapoor.merchantsguidetogalaxy.util;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserInputFileReader {
 
-	public List<String> read(String fileName) throws IOException, URISyntaxException {
-
-		File file;
-
-		if (fileName != null)
-			file = new File(fileName);
-		else {
-			URL url = getClass().getClassLoader().getResource("default-user-input.txt");
-			if (url == null)
-				throw new FileNotFoundException();
-
-			file = new File(url.toURI());
+	public List<String> read(String fileName) throws IOException {
+		
+		List<String> strList = new ArrayList<>();
+		
+		InputStream is = (fileName != null) ? new FileInputStream(fileName) : getClass().getResourceAsStream("/default-user-input.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			strList.add(line);
 		}
-
-		return Files.readAllLines(file.toPath(), Charset.defaultCharset());
+		
+		return strList;
 	}
 }
