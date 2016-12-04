437d8f63ee58a91f620024724b3f1b53a724b177
Shivam Kapoor <mail@shivamkapoor.com>
2016-12-05 02:53:13 +0530

    project renamed

--- scala/page-selection-by-keyword-matching-solution-IV/src/main/scala/com/codingkapoor/pageselectionbykeyword/model/Query.scala
+++
@@ -1,7 +0,0 @@
-package com.codingkapoor.pageselectionbykeyword.model
-
-case class Query(id: String, keywords: List[(String, Int)])
-
-object Query {
-  val identifier = "Q"
-}
--- scala/page-selection-by-keyword-matching-solution-IV/src/main/scala/com/codingkapoor/pageselectionbykeyword/util/ResultBuilder.scala
+++
@@ -1,5 +0,0 @@
-package com.codingkapoor.pageselectionbykeyword.util
-
-trait ResultBuilder {
-  def buildResult(i: List[(String, List[String])])
-}
