59e3ad7e2526799882617264448ca39a6ab04cde
Shivam Kapoor <mail@shivamkapoor.com>
2016-12-05 02:45:53 +0530

    page-selection-by-keyword-matching-solution-II/

--- scala/page-selection-by-keyword-matching-solution-II/src/main/scala/com/codingkapoor/pageselectionbykeyword/selector/PageSelector.scala
+++
@@ -1,9 +0,0 @@
-package com.codingkapoor.pageselectionbykeyword.selector
-
-trait PageSelector {
-  
-  final val maxPageSelections = 5
-
-  def pageSelectionByKeyword(fileName: Option[String]): List[String]
-  def display(result: List[String]) = result foreach { println(_) }
-}
