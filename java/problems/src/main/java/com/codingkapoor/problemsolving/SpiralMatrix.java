42ca0c6edbfef120f1e717ca062fe92535cde514
Shivam Kapoor <mail@shivamkapoor.com>
2016-12-29 16:13:16 +0530

    SpiralMatrix refactoring & test cases

--- java/problems/src/main/java/com/codingkapoor/problemsolving/SpiralMatrix.java
+++ java/problems/src/main/java/com/codingkapoor/problemsolving/SpiralMatrix.java
@@ -2,11 +2,12 @@ package com.codingkapoor.problemsolving;
 
 public class SpiralMatrix {
 
-	private int[] compute(int arr[][]) {
+	public int[] compute(int arr[][]) {
 
 		int rows = arr.length;
 		int columns = (rows > 0) ? arr[0].length : 0;
 
+		// level = UpperBound(Rows/2)
 		int level = (int) Math.ceil((double) rows / 2), r = 0;
 
 		int[] result = new int[(rows * columns)];
@@ -15,28 +16,28 @@ public class SpiralMatrix {
 
 		while (r < level) {
 
-			// traverse right
+			// traverse right: [R, C + 1]
 			for (int t = j + r; t < columns - r; t++) {
 				result[count] = arr[i][t];
 				count++;
 				j = t;
 			}
 
-			// traverse down
+			// traverse down: [R + 1, C]
 			for (int t = i + 1; t < rows - r; t++) {
 				result[count] = arr[t][j];
 				count++;
 				i = t;
 			}
 
-			// traverse left
+			// traverse left: [R, C - 1]
 			for (int t = j - 1; t >= 0 + r; t--) {
 				result[count] = arr[i][t];
 				count++;
 				j = t;
 			}
 
-			// traverse up
+			// traverse up: [R - 1, C]
 			for (int t = i - 1; t > 0 + r; t--) {
 				result[count] = arr[t][j];
 				count++;
@@ -53,15 +54,14 @@ public class SpiralMatrix {
 		return result;
 	}
 
-	private boolean validate(int arr[][]) {
+	public void isValidMatrix(int arr[][]) throws Exception {
 		int rows = arr.length;
 		int columns = arr[0].length;
 		for (int i = 1; i < rows; i++) {
 			if (arr[i].length != columns) {
-				return false;
+				throw new Exception("Invalid matrix!");
 			}
 		}
-		return true;
 	}
 
 	private void display(int arr[]) {
@@ -71,14 +71,17 @@ public class SpiralMatrix {
 	}
 
 	public static void main(String[] args) throws Exception {
+
+		// (0,0) (0,1) (0,2) (0,3) (0,4)
+		// (1,0) (1,1) (1,2) (1,3) (1,4)
+		// (2,0) (2,1) (2,2) (2,3) (2,4)
+		// (3,0) (3,1) (3,2) (3,3) (3,4)
+
 		int arr[][] = { { 1, 2, 3, 4, 5 }, { 14, 15, 16, 17, 6 }, { 13, 20, 19, 18, 7 }, { 12, 11, 10, 9, 8 } };
 
 		SpiralMatrix s = new SpiralMatrix();
 
-		if (s.validate(arr)) {
-			s.display(s.compute(arr));
-		} else
-			throw new Exception("Invalid matrix!");
-
+		s.isValidMatrix(arr);
+		s.display(s.compute(arr));
 	}
 }
--- java/problems/src/test/java/com/codingkapoor/problemsolving/SpiralMatrixTest.java
+++ java/problems/src/test/java/com/codingkapoor/problemsolving/SpiralMatrixTest.java
@@ -1,5 +1,59 @@
 package com.codingkapoor.problemsolving;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SpiralMatrixTest {
 
+	SpiralMatrix s = new SpiralMatrix();
+
+	// c < r
+	// { 1, 5, 6, 7 }
+	// { 2, 11, 12, 10 }
+	// { 3, 13, 14, 15 }
+	// { 4, 20, 19, 18 }
+	// { 8, 3, 9, 11 }
+
+	@Test
+	public void testWhenColumnsCountIsLessThanRowsCount() {
+		int arr[][] = { { 1, 5, 6, 7 }, { 2, 11, 12, 10 }, { 3, 13, 14, 15 }, { 4, 20, 19, 18 }, { 8, 3, 9, 11 } };
+		int[] res = s.compute(arr);
+
+		Assert.assertArrayEquals(res, new int[] { 1, 5, 6, 7, 10, 15, 18, 11, 9, 3, 8, 4, 3, 2, 11, 12, 14, 19, 20, 13 });
+	}
+
+	// c > r
+	// { 1, 5, 6, 7, 8 }
+	// { 2, 11, 12, 10, 9 }
+	// { 3, 13, 14, 15, 7 }
+	// { 4, 20, 19, 18, 16 }
+
+	@Test
+	public void testWhenColumnsCountIsMoreThanRowsCount() {
+		int arr[][] = { { 1, 5, 6, 7, 8 }, { 2, 11, 12, 10, 9 }, { 3, 13, 14, 15, 7 }, { 4, 20, 19, 18, 16 }, { 8, 3, 9, 11, 15 } };
+		int[] res = s.compute(arr);
+
+		Assert.assertArrayEquals(res, new int[] { 1, 5, 6, 7, 8, 9, 7, 16, 15, 11, 9, 3, 8, 4, 3, 2, 11, 12, 10, 15, 18, 19, 20, 13, 14 });
+	}
+
+	// c = r
+	// { 1, 5, 6, 7, 8 }
+	// { 2, 11, 12, 10, 9 }
+	// { 3, 13, 14, 15, 7 }
+	// { 4, 20, 19, 18, 16 }
+	// { 8, 3, 9, 11, 15 }
+
+	@Test
+	public void testWhenColumnsCountIsEqualToRowsCount() {
+		int arr[][] = { { 1, 5, 6, 7, 8 }, { 2, 11, 12, 10, 9 }, { 3, 13, 14, 15, 7 }, { 4, 20, 19, 18, 16 } };
+		int[] res = s.compute(arr);
+
+		Assert.assertArrayEquals(res, new int[] { 1, 5, 6, 7, 8, 9, 7, 16, 18, 19, 20, 4, 3, 2, 11, 12, 10, 15, 14, 13 });
+	}
+
+	@Test(expected = Exception.class)
+	public void testIfExceptionThrownForInvalidMatrix() throws Exception {
+		int arr[][] = { { 1, 5, 6, 7, 8 }, { 2, 11, 10, 9 }, { 3 }, { 4, 20, 19, 18, 16 } };
+		s.isValidMatrix(arr);
+	}
 }
